1de228f4b7c43d4f783664b9666f33f3dff64bcd
robinson-1985 <https://github.com/robinson-1985>
2021-03-24 15:32:19 -0300

    Adicionando novos exemplos

--- src/main/java/one/digitalinnovation/list/maps/ExemploHashMap.java
+++ src/main/java/one/digitalinnovation/list/maps/ExemploHashMap.java
@@ -2,9 +2,11 @@ package one.digitalinnovation.list.maps;
 
 import java.util.HashMap;
 import java.util.Map;
+
 public class ExemploHashMap {
     public static void main(String[] args) {
         Map<String, Integer> campeoesMundialFifa = new HashMap<>();
+
         // Adiciona os campeões mundias fifa no mapa
         campeoesMundialFifa.put("Brasil", 5);
         campeoesMundialFifa.put("Alemanha", 4);
@@ -28,22 +30,33 @@ public class ExemploHashMap {
 
         // Retorna se existe ou não um campeão França
         System.out.println(campeoesMundialFifa.containsKey("França"));
+
         // Remove o campeão França
         campeoesMundialFifa.remove("França");
+
         // Retorna se existe ou não um campeão França
         System.out.println(campeoesMundialFifa.containsKey("França"));
+
         // Retorna se existe ou não alguma seleção hexa campeã
         System.out.println(campeoesMundialFifa.containsValue(6));
+
         // Retorna o tamanho do mapa
         System.out.println(campeoesMundialFifa.size());
+
         System.out.println(campeoesMundialFifa);
+
         // Navega nos registros do mapa
         for (Map.Entry<String, Integer> entry : campeoesMundialFifa.entrySet()) {
+
             System.out.println(entry.getKey() + " -- " + entry.getValue());
+
         }
+
         // Navega nos registros do mapa
         for (String key : campeoesMundialFifa.keySet()) {
+
             System.out.println(key + " -- " + campeoesMundialFifa.get(key));
+
         }
 
         System.out.println(campeoesMundialFifa);
